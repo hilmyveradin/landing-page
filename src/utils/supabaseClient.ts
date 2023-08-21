44d246b722c24cf77f3323310deee0a3272f4294
hilmyveradin <hilmyveradin@gmail.com>
2023-08-21 20:13:52 +0700

    fix: modify error handling on process

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
+if (
+  !process.env.NEXT_PUBLIC_SUPABASE_LINK ||
+  !process.env.NEXT_PUBLIC_SUPABASE_KEY
+) {
+  throw new Error('Supabase environment variables are not set!');
+}
+
 export const supabase = createClient(
-  `${process.env.NEXT_PUBLIC_SUPABASE_LINK}`,
-  `${process.env.NEXT_PUBLIC_SUPABASE_KEY}`
+  process.env.NEXT_PUBLIC_SUPABASE_LINK,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
