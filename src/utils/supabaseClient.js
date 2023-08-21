eb7b61e28fe13e84752becc86c186c805848d029
hilmyveradin <hilmyveradin@gmail.com>
2023-08-21 12:00:26 +0700

    feat: modify the supabaseClient

--- src/utils/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://osblcogbuslhwrlmfeei.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zYmxjb2didXNsaHdybG1mZWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzOTkzMzAsImV4cCI6MjAwNjk3NTMzMH0.nFbqBBYR0uAKXh78YWW4shIXbNEuw3j232fXEztrj8s');
